4592e07daf3cbad4d7ddc727f0d4c92a4f1d5c09
Tao Sharma <taosharma@gmail.com>
2020-02-14 10:28:44 +0000

    added card classes

--- loveLetter.js
+++ loveLetter.js
@@ -9,58 +9,41 @@
 
 //Alex's suggestion: design each card as a constructer object with inbuilt functions.
 
-function Card(name) {
-  this.name = name;
-}
+//To do: 1. Implement handmaid function so that player is protected. 2. Refactor common play state changes into their own functions. 3. Finish logic for cards.
 
-class Guard extends Card {
-  constructor() {
-    super();
-    this.name = "guard";
-    this.value = 1;
-    this.action = function(opponentHand, opponentDiscard) {
-      //This function resolves the Guard card action. It lets a player guess the card in their opponents hand. If they guess correctly, their opponent discards the card and is out of the game.
-      let playerGuess = prompt("Guess your opponent's card.").value;
-      if (playerGuess === guard.type) {
-        playerGuess = prompt("Cannot guess guard. Try again.").value;
-      }
-      if (playerGuess === opponentHand[0]) {
-        opponentDiscard.push(opponentHand[0]);
-        opponentHand.splice(0, 1);
-        return opponentHand, opponentDiscard;
-      } else return;
-    };
-  }
-}
+const { Card, Guard, Priest, Baron } = require("./loveLetterCards.js");
 
-const guard = { value: 1, type: "guard" };
-const priest = { value: 2, type: "priest" };
-const baron = { value: 3, type: "baron" };
+const guard1 = new Guard();
+const guard2 = new Guard();
+const priest1 = new Priest();
+const priest2 = new Priest();
+const baron1 = new Baron();
+const baron2 = new Baron();
 
-let deckOfCards = [
-  guard,
-  guard,
-  guard,
-  guard,
-  guard,
-  priest,
-  priest,
-  baron,
-  baron,
-  "Handmaid",
-  "Handmaid",
-  "Prince",
-  "Prince",
-  "King",
-  "Countess",
-  "Princess"
-];
+// let deckOfCards = [
+//   guard,
+//   guard,
+//   guard,
+//   guard,
+//   guard,
+//   priest,
+//   priest,
+//   baron,
+//   baron,
+//   "Handmaid",
+//   "Handmaid",
+//   "Prince",
+//   "Prince",
+//   "King",
+//   "Countess",
+//   "Princess"
+// ];
 
-let player1Hand = [Guard, Guard];
+let player1Hand = [baron1, guard2];
 
 let player1Discard = [];
 
-let player2Hand = [baron];
+let player2Hand = [guard1];
 
 let player2Discard = [];
 
@@ -69,7 +52,6 @@ let chosenCard = 0;
 function getRandomInt(max) {
   //Function which can choose a random whole number with a max limit.
   return Math.floor(Math.random() * max);
-  ``;
 }
 
 function dealCards() {
@@ -80,9 +62,9 @@ function dealCards() {
 
 function endGame() {
   //This function ends the game when a player has no cards in their hand and tries to draw a card.
-  if (player1Hand === []) {
+  if (player1Hand == []) {
     console.log("Player 2 wins!");
-  } else if (player2Hand === []) {
+  } else if (player2Hand == []) {
     console.log("Player 1 wins!");
   }
 }
@@ -106,46 +88,18 @@ function playCard(
   //This function lets the player play a card by putting it in their discard pile and resolving its action.
   playerDiscard.push(playerHand[chosenCard]);
   playerHand.splice(chosenCard, 1);
-  playerDiscard[playerDiscard.length - 1].action(opponentHand, opponentDiscard);
-
-  // if (playerDiscard[playerDiscard.length - 1] === Guard) {
-  //   playGuard(opponentHand, opponentDiscard);
-  // }
-  // if (playerDiscard[0] === priest) {
-  //   playPriest(opponentHand);
-  // }
-  // if (opponentHand === [] || playerHand === []) {
-  //   endGame();
-  // }
+  playerDiscard[playerDiscard.length - 1].action(
+    playerHand,
+    opponentHand,
+    playerDiscard,
+    opponentDiscard
+  );
+  if (opponentHand == [] || playerHand == []) {
+    endGame();
+  }
   return playerHand, opponentHand, playerDiscard, opponentDiscard;
 }
 
-// function playGuard(opponentHand, opponentDiscard) {
-//   //This function resolves the Guard card action. It lets a player guess the card in their opponents hand. If they guess correctly, their opponent discards the card and is out of the game.
-//   let playerGuess = baron; //Enter guessed card here.
-//   if (playerGuess === guard.type) {
-//     playerGuess = prompt("Guess your opponent's card.");
-//   }
-//   if (playerGuess === opponentHand[0]) {
-//     opponentDiscard.push(opponentHand[0]);
-//     opponentHand.splice(0, 1);
-//     return opponentHand, opponentDiscard;
-//   } else return;
-// }
-
-function playPriest(opponentHand) {
-  //This function resolves the Priest card action. It lets a player see the card in their opponents hand.
-  alert(opponentHand[0]);
-}
-
-function playBaron(playerHand, opponentHand, playerDiscard, opponentDiscard) {
-  //This function resolves the Baron card action. It lets a player compare the value of the remaining card in their hand with the value of the card in their opponent's hand. Whichever player has the lower value discards their card and is out of the game. If they are equal both the player and their opponent keep their cards, but now know what cards each other have.
-  if (playerHand[0][1] > opponentCard.value) {
-    opponentDiscard.push(opponentHand[0]);
-    opponentHand.splice(0, 1);
-  }
-}
-
-// console.log(0, player1Hand, player2Hand, player1Discard, player2Discard);
-// playCard(0, player1Hand, player2Hand, player1Discard, player2Discard);
-// console.log(0, player1Hand, player2Hand, player1Discard, player2Discard);
+// console.log(player1Hand, player2Hand, player1Discard, player2Discard);
+playCard(0, player1Hand, player2Hand, player1Discard, player2Discard);
+console.log(player1Hand, player2Hand, player1Discard, player2Discard);
